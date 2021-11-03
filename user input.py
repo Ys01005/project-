efac07d81b0e26274cf005b84e8521aa96a8ddbe
Ys01005 <93677795+Ys01005@users.noreply.github.com>
2021-11-03 20:57:15 +0200

    Add files via upload

---
+++ user input.py
@@ -0,0 +1,13 @@
+a = 1140671485 
+c= 12820163
+m = 2**24
+seed = int(input("Seed: "))
+def LCG(rando):
+    seed = int(((a*rando)+c)%m)
+    return seed
+print("LCG random numbers generator")
+for X in range(1,21):
+    print(X,":",LCG(seed))
+    seed = LCG(seed)
+
+   
